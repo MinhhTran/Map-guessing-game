0f167b54f2f52bdbe2786db4d36d1a295983e352
Tran Minh <minh.q.tran@aalto.fi>
2026-04-27 22:32:03 +0300

    Add mouse hover paint feature

--- CountryItem.py
+++ CountryItem.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QGraphicsPathItem
 from PyQt6.QtGui import QColor, QPen
+from PyQt6.QtCore import Qt
 
 class CountryItem(QGraphicsPathItem):
     def __init__(self, country_data, path):
@@ -11,6 +12,10 @@ class CountryItem(QGraphicsPathItem):
         self.setPen(QPen(QColor("white"), 0.5))
         self.setBrush(QColor("lightgray"))
         
+        # Define your color palette
+        self.default_color = QColor("lightgray")
+        self.hover_color = QColor("lightblue")
+
         # Allow interaction
         self.setAcceptHoverEvents(True)
 
@@ -18,3 +23,18 @@ class CountryItem(QGraphicsPathItem):
         #logic for when a user clicks the country.
         print(f"Clicked on: {self.country_data.get_info()}")
         super().mousePressEvent(event)
+
+    def hoverEnterEvent(self, event):
+        # trigger when mouse enter the country
+        self.setBrush(self.hover_color)
+        super().hoverEnterEvent(event)
+
+    def hoverLeaveEvent(self, event):
+        # trigger when mouse leave the country
+        self.setBrush(self.default_color)
+        super().hoverLeaveEvent(event)
+
+    def change_color(self, color_name):
+        # Permanently change country color (for quiz mode)
+        self.default_color = QColor(color_name)
+        self.setBrush(self.default_color)
